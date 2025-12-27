b7e9d9dbf597762d81221a4580192c998714de45
Liyan Zhao <return65535@qq.com>
2025-12-28 00:38:12 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use async_stream::__private::AsyncStream;
+use axum::{body::Body, http::{HeaderMap, HeaderValue, StatusCode}, response::{IntoResponse, Response}, routing::post, Error, Router};
+use std::time::Duration;
+
+async fn handler(
+) -> Result<Response, (StatusCode, ())> {
+    let output_stream: AsyncStream<Result<bytes::Bytes, Error>, _> = async_stream::stream! {
+        yield Ok(bytes::Bytes::from_static(b"event:no-space-here\ndata:message1\n\n"));
+        tokio::time::sleep(Duration::from_secs(10)).await;
+        yield Ok(bytes::Bytes::from_static(b"event:final-event\ndata:message2\n\n"));
+    };
+
+    let body = Body::from_stream(output_stream);
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        "content-type",
+        HeaderValue::from_static("text/event-stream"),
+    );
+    headers.insert("cache-control", HeaderValue::from_static("no-cache"));
+    headers.insert("connection", HeaderValue::from_static("keep-alive"));
+
+    Ok((headers, body).into_response())
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/api/sse", post(handler));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
